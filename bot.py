9ca4800a44e80ef3237d992704e07e2862160d0c
Elizaveta Karachinskaya <elizavetakarachinskayaa@gmail.com>
2016-11-19 11:36:59 +0300

    izmenili bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,19 @@
 # для работы с ботом
+__author__ = 'Elizaveta Karachinskaya'
+import telebot
+import config
+#соединение с ботом
+bot = telebot.TeleBot(config.token)
+
+#Отвечает на команды
+@bot.message_handler(commands=['start', 'help'])
+def send_welcome(message):
+    bot.reply_to(message, "Привет, как дела?")
+
+#Отвечает на все сообщения по умолчанию
+@bot.message_handler(func=lambda m: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
+
+#Запуск бота
+bot.polling()
