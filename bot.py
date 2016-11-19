eba1ac4f25c191d63af5c34363c87c5e9967b5d6
Elizaveta Karachinskaya <elizavetakarachinskayaa@gmail.com>
2016-11-19 16:19:21 +0300

    izmenenie baz dann

--- bot.py
+++ bot.py
@@ -2,13 +2,24 @@
 __author__ = 'Elizaveta Karachinskaya'
 import telebot
 import config
+import db
 #соединение с ботом
 bot = telebot.TeleBot(config.token)
 
 #Отвечает на команды
-@bot.message_handler(commands=['start', 'help'])
+@bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет, как дела?")
+    query = "SELECT  answers FROM commands WHERE id=1"
+    result = db.query(query)
+
+
+    bot.reply_to(message, result)
+
+@bot.message_handler(commands=['help'])
+def send_welcome(message):
+    query = "SELECT  answers FROM commands WHERE id=2" #запрос из бд для справки help
+    result = db.query(query)
+    bot.reply_to(message, result)
 
 #Отвечает на все сообщения по умолчанию
 @bot.message_handler(func=lambda m: True)
