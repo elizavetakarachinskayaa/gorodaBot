bbff581be6ec6dcac86c6ec81fdc9e2f218ed876
Elizaveta Karachinskaya <elizavetakarachinskayaa@gmail.com>
2016-12-24 11:49:45 +0300

    gotovo2

---
+++ config.py
@@ -0,0 +1,10 @@
+__author__ = '12345'
+"""
+#Сюда выносим настройки проекта
+Documentation
+"""
+
+# TelegramBot key
+token = "260005042:AAE6Ualn3-mRhPyYJ-E6ZOjJrFM5Rsp5XK0"
+#Database sqlite
+database_name = "bot.sqlite3"
